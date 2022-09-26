ef24b0ac35c95aa793217f38b7eaef85b946bea3
cdw <1370909027@qq.com>
2022-09-26 22:16:50 +0800

    #5

---
+++ useIshover.jsx
@@ -0,0 +1,25 @@
+import { Ref, useRef, useState, useCallback } from 'react'
+
+export function useHover() {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const handleMouseEnter = useCallback(() => setIsHovered(true), []);
+  const handleMouseLeave = useCallback(() => setIsHovered(false), []);
+
+  const ref = useRef();
+  const callbackRef = useCallback(node => {
+    if (ref.current) {
+      ref.current.removeEventListener('mouseenter', handleMouseEnter);
+      ref.current.removeEventListener('mouseleave', handleMouseLeave);
+    }
+
+    ref.current = node;
+
+    if (ref.current) {
+      ref.current.addEventListener('mouseenter', handleMouseEnter);
+      ref.current.addEventListener('mouseleave', handleMouseLeave);
+    }
+  }, [handleMouseEnter, handleMouseLeave]);
+
+  // your code here
+  return [callbackRef, isHovered];
+}
