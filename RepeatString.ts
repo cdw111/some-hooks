b5af380af67daf478fa32e19b7fe2c8eaf78c19e
cdw <1370909027@qq.com>
2022-11-21 20:16:56 +0800

    #54

---
+++ RepeatString.ts
@@ -0,0 +1,4 @@
+type RepeatString<T extends string, C extends number, S extends any[] = [],P extends string = ''> =
+S["length"] extends C ?
+P :
+RepeatString<T,C,[...S,0],`${T}${P}`>
--- index.ts
+++ index.ts
@@ -89,3 +89,67 @@ Divide<Subtract<A,B>,B,[...S,0]>
 
 
 type test1 = Divide<1,0>
+
+
+
+type MyCapitalize<T extends string> = T extends `${infer F}${infer P}` ? `${Uppercase<F>}${P}` : never;
+type aaa = MyCapitalize<''>
+
+
+type Split<S extends string, D extends string, T extends string = '', P extends string[] = []> =
+S extends `${infer F}${infer L}` ?
+S extends `${D}${infer L}` ?
+Split<L,D,'',[...P,T]> :
+Split<L,D,`${T}${F}`,[...P]> :
+[...P,T]
+
+type asd = Split<'bfe.bfe.bfe', 'bfe'>
+
+type SnakeCase<S extends string,T extends string = ''> =
+S extends `${infer F}${infer P}` ?
+T extends '' ?
+SnakeCase<P,`${T}${F}`> :
+F extends Uppercase<F> ?
+SnakeCase<P,`${T}_${F}`> :
+SnakeCase<P,`${T}${F}`> :
+Lowercase<T>
+
+type A = SnakeCase<'BigFrontEnd'>
+
+
+type CamelCase<S extends string,T extends string = ''> =
+S extends `${infer F}${infer L}${infer P}` ?
+T extends '' ?
+CamelCase<`${L}${P}`,`${T}${Uppercase<F>}`> :
+F extends '_' ?
+CamelCase<P,`${T}${Uppercase<L>}`> :
+CamelCase<`${L}${P}`,`${T}${F}`> :
+`${T}${S}`
+
+type B = CamelCase<'big_front_end'> 
+
+
+type StringToNumber<S extends string,T extends any[] = []> = `${T["length"]}` extends S ? T["length"] : StringToNumber<S, [...T,0]>
+type Abs<N extends number> = `${N}` extends `-${infer P}` ? StringToNumber<P> : N
+
+type ObjectPaths<O extends Record<string, any>> = {
+  [K in keyof O]: K extends string 
+                  ? O[K] extends Record<string, any> ? `${K}.${ObjectPaths<O[K]>}` : `${K}`
+                  : never
+}[keyof O];
+
+
+type DiffKeys<
+  A extends Record<string, any>,
+  B extends Record<string, any>
+> = Exclude<keyof A, keyof B> | Exclude<keyof B, keyof A>
+
+type MapStringUnionToObjectUnion<U extends string> = { [k in U]: { value: k } }[U]
+type UndefinedToNull<T> = T extends undefined ? null :
+    T extends [infer A] ? [UndefinedToNull<A>] :
+    T extends [infer A, ...infer B] ? [UndefinedToNull<A>, ...UndefinedToNull<B>] :
+    T extends (infer R)[] ? UndefinedToNull<R>[]:
+    T extends {} ? { [k in keyof T]: UndefinedToNull<T[k]>}:
+    T extends infer R | undefined ? R | null : never;
+
+    
